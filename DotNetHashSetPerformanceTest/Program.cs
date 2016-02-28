7d27e217614b76c5526503e42171d699186c9c22
quails4Eva <dpetchey@gmail.com>
2016-02-28 20:24:40 +0000

    Add enum test. Add results and some thoughts

--- DotNetHashSetPerformanceTest/Program.cs
+++ DotNetHashSetPerformanceTest/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNetHashSetPerformanceTest {
+
     class Program {
+
+        private const string filePath = "C:\\Temp\\HashSetVsArrayPerformanceOutput.txt";
+
+        private enum TestEnum {
+            Test0 = 0,
+            Test1,
+            Test2,
+            Test3,
+            Test4,
+            Test5,
+            Test6,
+            Test7,
+            Test8,
+            Test9,
+
+            Test10,
+            Test11,
+            Test12,
+            Test13,
+            Test14,
+            Test15,
+            Test16,
+            Test17,
+            Test18,
+            Test19,
+        }
+
         static void Main(string[] args) {
 
             const int numberOfIterations = 2000000;
@@ -14,15 +41,24 @@ namespace DotNetHashSetPerformanceTest {
 
             for (int numElements = 0; numElements < 10; numElements++) {
 
-                Console.WriteLine($"Elements: {numElements}, int, in range, result: {ProfileTime<int>(numberOfIterations, numberOfRepeats, numElements, true)}");
-                Console.WriteLine($"Elements: {numElements}, int, out of range, result: {ProfileTime<int>(numberOfIterations, numberOfRepeats, numElements, false)}");
+                Program.WriteLine($"Elements: {numElements}, int, in range, result: {ProfileTime<int>(numberOfIterations, numberOfRepeats, numElements, true)}");
+                Program.WriteLine($"Elements: {numElements}, int, out of range, result: {ProfileTime<int>(numberOfIterations, numberOfRepeats, numElements, false)}");
 
-                Console.WriteLine($"Elements: {numElements}, string, in range, result: {ProfileTime<string>(numberOfIterations, numberOfRepeats, numElements, true)}");
-                Console.WriteLine($"Elements: {numElements}, string, out of range, result: {ProfileTime<string>(numberOfIterations, numberOfRepeats, numElements, false)}");
+                Program.WriteLine($"Elements: {numElements}, string, in range, result: {ProfileTime<string>(numberOfIterations, numberOfRepeats, numElements, true)}");
+                Program.WriteLine($"Elements: {numElements}, string, out of range, result: {ProfileTime<string>(numberOfIterations, numberOfRepeats, numElements, false)}");
+
+                Program.WriteLine($"Elements: {numElements}, enum, in range, result: {ProfileTime<TestEnum>(numberOfIterations, numberOfRepeats, numElements, true)}");
+                Program.WriteLine($"Elements: {numElements}, enum, out of range, result: {ProfileTime<TestEnum>(numberOfIterations, numberOfRepeats, numElements, false)}");
                 Console.WriteLine();
             }
             Console.WriteLine("Done");
             Console.ReadLine();
+        }
+
+        private static void WriteLine(string val) {
+
+            Console.WriteLine(val);
+            File.AppendAllText(filePath, val + Environment.NewLine);
         }
 
         private static ProfileResult ProfileTime<T>(int numberOfIterations, int numberOfRepeats, int numElements, bool toCheckIsInRange) {
@@ -69,8 +105,9 @@ namespace DotNetHashSetPerformanceTest {
                 results[j] = new ProfileResult(arrayTime, hashSetTime);
             }
 
-            var arrayAverage = results.Average(r => r.ArrayTime);
-            var hashSetAverage = results.Average(r => r.HashSetTime);
+            // We skip ~ 10% of repeats to reduce the impact of CPU spikes.
+            var arrayAverage = results.OrderByDescending(r => r.ArrayTime).Skip(numberOfRepeats / 10).Average(r => r.ArrayTime);
+            var hashSetAverage = results.OrderByDescending(r => r.HashSetTime).Skip(numberOfRepeats / 10).Average(r => r.HashSetTime);
 
             return new ProfileResult(arrayAverage, hashSetAverage);
         }
@@ -79,7 +116,7 @@ namespace DotNetHashSetPerformanceTest {
 
             var ints = Enumerable.Range(0, numElements).Cast<object>();
 
-            if (typeof(T) == typeof(int)) {
+            if (typeof(T) == typeof(int) || typeof(T) == typeof(TestEnum)) {
                 
             }
             else if (typeof(T) == typeof(string)) {
@@ -95,9 +132,10 @@ namespace DotNetHashSetPerformanceTest {
 
         private static T GenerateValue<T>(Random numberGenerator, int numElements, bool toCheckIsInRange) {
 
-            var toCheck = (object)(numberGenerator.Next(numElements) + (toCheckIsInRange ? 0 : numElements));
+            // We use object because we may convert it to another type after int (could use a different var)
+            object toCheck = numberGenerator.Next(numElements) + (toCheckIsInRange ? 0 : (numElements == 0 ? 1 : numElements));
 
-            if (typeof(T) == typeof(int)) {
+            if (typeof(T) == typeof(int) || typeof(T) == typeof(TestEnum)) {
 
             }
             else if (typeof(T) == typeof(string)) {
